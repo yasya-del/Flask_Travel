9b0c6c93a7d1c13ce0ef9a279596577604937f12
sofatimofeeva <timofeevasofa26@gmail.com>
2025-04-16 23:36:36 +0300

    + navbar + выбор стран

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, url_for, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def title():
-    return 'Тут будет главная страница сайта'
+    return render_template('base.html')
+
+@app.route('/countries')
+def choose_countries():
+    return render_template('countries.html')
+
+@app.route('/country/<name>')
+def country(name):
+    return render_template(f'{name}.html')
 
 
 if __name__ == '__main__':
