bdb44b1c2814eae022bb41141847fadf0d5228c0
вася <vasilisa.zaitseva2010@gmail.com>
2025-04-17 12:50:19 +0300

    Add files via upload

---
+++ data/users.py
@@ -0,0 +1,25 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
